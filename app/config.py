4be131a7b7944a5387d7126391a8fabe4fd39067
Jim <dorajim15@gmail.com>
2020-11-29 10:58:00 +0800

    feat: add basic setting

---
+++ app/config.py
@@ -0,0 +1,13 @@
+import os
+
+# use k8s config-maps to override this file
+DEBUG = bool(os.environ.get('DEBUG', False))
+TEST = bool(os.environ.get('TEST', False))
+
+SERVICE_NAME = 'chatbot-service'
+
+HTTP_PORT = 8080
+REDIS_NODE = os.environ.get('REDIS_NODE', '')
+
+# celery queue names
+CHATBOT_SERVICE_CELERY_QUEUE = 'chatbot-service'
---
+++ app/settings.py
@@ -0,0 +1,39 @@
+import secret as sc
+import config as cf
+
+# configs
+DEBUG = cf.DEBUG
+TEST = cf.TEST
+
+SERVICE_NAME = cf.SERVICE_NAME
+
+HTTP_PORT = cf.HTTP_PORT
+REDIS_NODE = cf.REDIS_NODE
+
+# celery queue names
+CHATBOT_SERVICE_CELERY_QUEUE = cf.CHATBOT_SERVICE_CELERY_QUEUE
+
+# secrets
+LINE_CHANNEL_ACCESS_TOKEN = sc.LINE_CHANNEL_ACCESS_TOKEN
+LINE_CHANNEL_SECRET = sc.LINE_CHANNEL_SECRET
+
+# MAIN_DB_URL = 'postgresql+psycopg2://{username}:{password}@{ip}/{db}'.format(
+#     username=sc.MAIN_DB['username'],
+#     password=sc.MAIN_DB['password'],
+#     ip=sc.MAIN_DB['ip'],
+#     db=sc.MAIN_DB['db']
+# )
+
+# SLAVE_DB_URL = 'postgresql+psycopg2://{username}:{password}@{ip}/{db}'.format(
+#     username=sc.SLAVE_DB['username'],
+#     password=sc.SLAVE_DB['password'],
+#     ip=sc.SLAVE_DB['ip'],
+#     db=sc.SLAVE_DB['db']
+# )
+
+# TEST_DB_URL = 'postgresql+psycopg2://{username}:{password}@{ip}/{db}'.format(
+#     username=sc.TEST_DB['username'],
+#     password=sc.TEST_DB['password'],
+#     ip=sc.TEST_DB['ip'],
+#     db=sc.TEST_DB['db']
+# )
