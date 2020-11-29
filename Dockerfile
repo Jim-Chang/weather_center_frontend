03e7e55b21c98493f3121aa281bafaa3bfa4c53e
Jim <dorajim15@gmail.com>
2020-11-29 10:37:48 +0800

    init commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:3.8.5-slim
+EXPOSE 8080
+
+RUN groupadd -r chatbot
+RUN useradd -r -u 1010 -g chatbot chatbot
+
+RUN apt-get update && \
+	apt-get upgrade -yqq && \
+	apt-get install -y build-essential libpq-dev python3-dev && \
+	apt-get clean -yqq && \
+	apt-get autoclean -yqq && \
+	apt-get autoremove -yqq && \
+	rm -rf /var/cache/apt/archives/* /var/cache/apt/*.bin /var/lib/apt/lists/*
+
+WORKDIR /app
+COPY app/requirements.txt /app/requirements.txt 
+RUN pip install --trusted-host pypi.python.org -r requirements.txt
+COPY app /app
+
+RUN chown -R chatbot /app
+USER chatbot
+ENTRYPOINT ["sh", "entrypoint.sh"]
---
+++ app/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# http_adapter mode
+elif [ "$SERVE_MODE" = "http_adapter" ]
+then
+	echo "SERVE MODE: http_adapter"
+	uwsgi --ini uwsgi.ini
+
+# celery worker mode
+elif [ "$SERVE_MODE" = "celery_worker" ]
+then
+	echo "SERVE MODE: celery_worker"
+	celery --app celery_app worker --concurrency $CELERY_WORKER_NUM --queues pm-service --loglevel INFO
+
+# container only run migrate
+elif [ "$SERVE_MODE" = "run_migrate" ]
+then
+	echo "SERVE MODE: run_migrate"
+	alembic -c /app/db/migrations/alembic.ini upgrade head
+
+# all mode (for develop use. 如果沒有設定環境變數，預設也跑這裡)
+else
+	echo "SERVE MODE: all"
+	uwsgi --ini uwsgi.ini
+fi
