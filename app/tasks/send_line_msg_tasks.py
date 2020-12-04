9c85c0b7f5489981bdf7beca5b1912635319db55
Jim-Chang <dorajim15@gmail.com>
2020-12-04 12:40:45 +0800

    feat: modify send line msg task

--- app/tasks/send_line_msg_tasks.py
+++ app/tasks/send_line_msg_tasks.py
@@ -4,6 +4,10 @@ from celery_app import app
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
+# send_text_message 的包裝
+def async_send_text_message(reply_token: str, text_message: str):
+    send_text_message.apply_async(args=(reply_token, text_message,), queue=settings.CHATBOT_SERVICE_CELERY_QUEUE)
+
 @app.task
 def send_text_message(reply_token: str, text_message: str):
     line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
--- app/tests/tasks/test_send_line_msg_tasks.py
+++ app/tests/tasks/test_send_line_msg_tasks.py
@@ -11,3 +11,8 @@ from linebot.models import TextSendMessage
 def test_send_text_message(mock_method):
     tasks.send_text_message('fake token', 'fake msg')
     mock_method.assert_called_once_with('fake token', TextSendMessage(text='fake msg'))
+
+@patch('tasks.send_line_msg_tasks.send_text_message.apply_async')
+def test_async_send_text_message(mock_method):
+    tasks.async_send_text_message('fake token', 'fake msg')
+    mock_method.assert_called_once()
