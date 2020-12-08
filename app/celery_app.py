17c5535f6dbe4a3b6f4c0ef9cdc7dc87c2e13419
Jim-Chang <dorajim15@gmail.com>
2020-12-08 19:05:05 +0800

    feat: modify task route

--- app/celery_app.py
+++ app/celery_app.py
@@ -21,10 +21,10 @@ app.conf.update(
 app.conf.task_routes = {
     'tasks.send_line_msg_tasks.*': {'queue': settings.CELERY_CHATBOT_QUEUE_NAME},
     'tasks.weather_tasks.*': {'queue': settings.CELERY_CHATBOT_QUEUE_NAME},
+    'tasks.stock_tasks.*': {'queue': settings.CELERY_CHATBOT_QUEUE_NAME},
 
     'tasks.refresh_cache_for_wp_task.*': {'queue': settings.CELERY_DOWNLOAD_QUEUE_NAME},
     'tasks.download_and_upload_task.*': {'queue': settings.CELERY_DOWNLOAD_QUEUE_NAME},
-    'tasks.stock_tasks.*': {'queue': settings.CELERY_DOWNLOAD_QUEUE_NAME},
 }
 
 if __name__ == '__main__':
