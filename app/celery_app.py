db1fe9e8e459f2e71fa91e9ae5c620fe6d127c9e
Jim-Chang <dorajim15@gmail.com>
2020-12-11 18:24:28 +0800

    feat: add periodic task

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.schedules import crontab
 import settings
 
 app = Celery(
@@ -26,6 +27,14 @@ app.conf.task_routes = {
     'tasks.refresh_cache_for_wp_task.*': {'queue': settings.CELERY_DOWNLOAD_QUEUE_NAME},
     'tasks.download_and_upload_task.*': {'queue': settings.CELERY_DOWNLOAD_QUEUE_NAME},
 }
+
+app.conf.beat_schedule = {
+    'send-0050tw-price-after-close': {
+        'task': 'tasks.stock_tasks.send_0050tw_price_after_close_periodic_task',
+        'schedule': crontab(minute='10', hour='14', day_of_week='mon,tue,wed,thu,fri'),
+    },
+}
+app.conf.timezone = 'Asia/Taipei'
 
 if __name__ == '__main__':
     app.start()
