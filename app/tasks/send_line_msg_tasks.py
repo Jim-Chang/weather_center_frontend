72d7a413865d9ff76a20e4b3da2690491f4060e8
Jim-Chang <dorajim15@gmail.com>
2020-12-16 11:02:59 +0800

    fix: add try catch of line bot api

--- app/tasks/send_line_msg_tasks.py
+++ app/tasks/send_line_msg_tasks.py
@@ -1,6 +1,7 @@
 from typing import List
 import settings
 from celery_app import app
+from utils.log import logging
 
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
@@ -12,10 +13,13 @@ def async_send_text_message(user_id: str, text_message: str):
 @app.task
 def send_text_message(user_id: str, text_message: str):
     line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
-    line_bot_api.push_message(
-        user_id,
-        TextSendMessage(text=text_message)
-    )
+    try:
+        line_bot_api.push_message(
+            user_id,
+            TextSendMessage(text=text_message)
+        )
+    except Exception:
+        logging.error('send text message error', exc_info=True)
 
 # send_text_message 的包裝
 def async_send_multi_text_message(user_id: str, text_messages: List[str]):
@@ -27,7 +31,10 @@ def send_multi_text_message(user_id: str, text_messages: List[str]):
 
     messages = [TextSendMessage(text=text_message) for text_message in text_messages]
 
-    line_bot_api.push_message(
-        user_id,
-        messages
-    )
+    try:
+        line_bot_api.push_message(
+            user_id,
+            messages
+        )
+    except Exception:
+        logging.error('send text message error', exc_info=True)
