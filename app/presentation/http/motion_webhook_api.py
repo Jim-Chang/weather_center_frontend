82a5357e28cd37b3f1faa8a1bbdb303b99ee4773
Jim <dorajim15@gmail.com>
2021-01-04 12:41:49 +0800

    feat: add webhook for new record to send msg to line

--- app/presentation/http/motion_webhook_api.py
+++ app/presentation/http/motion_webhook_api.py
@@ -1,12 +1,27 @@
 from flask import Blueprint, request, make_response
 from utils.log import logging
 
+from domain.enums.type import RoleType
+
+from tasks.send_line_msg_tasks import async_send_text_message
+from utils import permission
+
 motion_webhook_api = Blueprint('motion_webhook_api', __name__)
 
+notify_users = permission._role_user_list_map.get(RoleType.admin)
 
 @motion_webhook_api.route('/record/new', methods=['POST'])
 def on_new_record():
-    data = request.json
-    print(data)
+    '''
+    {
+        'filename': '/var/lib/motioneye/Camera1/2021-01-04/12-16-03.mp4'
+    }
+    '''
+    data = request.json['filename'].split('/')
+    camera, date, filename = data[4], data[5], data[6]
+    message = '{} 偵測到動靜！\n日期：{}\n錄影檔名：{}'.format(camera, date, filename)
+    
+    for user_id in notify_users:
+        async_send_text_message(user_id, message)
 
     return make_response('', 200)
--- app/tests/presentation/http/test_motion_webhook_api.py
+++ app/tests/presentation/http/test_motion_webhook_api.py
@@ -1,9 +1,11 @@
 import pytest
+from unittest.mock import patch
 
 from adapter.http import get_app
 
 @pytest.mark.http
-def test_on_new_record():
+@patch('tasks.send_line_msg_tasks.send_text_message.apply_async')
+def test_on_new_record(mock_method):
 
     app = get_app()
     client = app.test_client()
@@ -11,8 +13,9 @@ def test_on_new_record():
     response = client.post(
         'webhook/motion/record/new',
         json={
-            'test': 'test_data'
+            'filename': '/var/lib/motioneye/Camera1/2021-01-04/12-16-03.mp4'
         }
     )
 
     assert response.status_code == 200
+    mock_method.assert_called_once_with(args=('Uebaac5edfee64f6b934a4d27b937cead', 'Camera1 偵測到動靜！\n日期：2021-01-04\n錄影檔名：12-16-03.mp4'))
