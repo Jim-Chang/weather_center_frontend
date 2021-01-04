ed38538da2d97f3775a8320668cc783524f8368a
Jim <dorajim15@gmail.com>
2021-01-04 16:07:48 +0800

    feat: add test

--- app/tests/infrastructure/service/test_motion_service.py
+++ app/tests/infrastructure/service/test_motion_service.py
@@ -83,3 +83,29 @@ def test_real_motion_service__get_detection_status__case_2():
 
     svc = RealMotionService()
     assert svc.get_detection_status(1) == MotionDetectionStatus.disable
+
+@pytest.mark.in_memory
+@responses.activate
+def test_real_motion_service__set_detection_status__case_1():
+    responses.add(
+        responses.GET,
+        'http://{}:7999/1/detection/start'.format(settings.MOTION_SERVER_IP),
+        body='Camera 1 Detection resumed\nDone \n',
+        status=200
+    )
+
+    svc = RealMotionService()
+    assert svc.set_detection_status(1, MotionDetectionStatus.enable) is True
+
+@pytest.mark.in_memory
+@responses.activate
+def test_real_motion_service__set_detection_status__case_2():
+    responses.add(
+        responses.GET,
+        'http://{}:7999/1/detection/pause'.format(settings.MOTION_SERVER_IP),
+        body='Camera 1 Detection paused\nDone \n',
+        status=200
+    )
+
+    svc = RealMotionService()
+    assert svc.set_detection_status(1, MotionDetectionStatus.disable) is True
