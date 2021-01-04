62a384eb1307ea12572e4bb32bca36ad09a64a43
Jim <dorajim15@gmail.com>
2021-01-04 16:24:11 +0800

    feat: add commands to translator

--- app/presentation/translator/text_command_translator.py
+++ app/presentation/translator/text_command_translator.py
@@ -5,15 +5,21 @@ from application.command.chat_bot_commands import (
     run_refresh_cache_for_wp_command,
     run_get_stock_price_command,
     run_get_user_id_command,
+    run_motion_start_detection_command,
+    run_motion_stop_detection_command,
+    run_motion_get_detection_status_command,
 )
 
 class TextCommandTranslator:
 
     # <command_key>: <command_func>
     command_map = {
+        '我是誰': run_get_user_id_command,
         '刷新部落格': run_refresh_cache_for_wp_command,
         '股票': run_get_stock_price_command,               # 指令範例 '股票 0050.tw' 
-        '我是誰': run_get_user_id_command,
+        '啟動偵測': run_motion_start_detection_command,
+        '關閉偵測': run_motion_stop_detection_command,
+        '偵測狀態': run_motion_get_detection_status_command,
     }
 
     def decode_command(self, command: str) -> Optional[Type]:
