67879a6dfcd660a91d71988440bf524160b92fdc
Jim <dorajim15@gmail.com>
2021-01-05 17:18:02 +0800

    feat: modify motion relate tasks

--- app/tasks/motion_tasks.py
+++ app/tasks/motion_tasks.py
@@ -11,50 +11,60 @@ from utils import permission
 def async_start_detection_task(user_id: str, reply_token: str, camera_id: int):
     start_detection_task.apply_async(args=(user_id, reply_token, camera_id))
 
-# 提供給 celery beat 呼叫使用
-@app.task
-def cron_start_detection_task():
-    notify_user_id = permission._role_user_list_map.get(RoleType.admin)[0]
-    start_detection_task(notify_user_id, None, 1)  # 目前只有一個 直接 hard code
-
 @app.task
 def start_detection_task(user_id: str, reply_token: Optional[str], camera_id: int):
     svc = RealMotionService()
     result = svc.set_detection_status(camera_id, MotionDetectionStatus.enable)
 
-    if result:
-        msg = '啟動動態偵測完成！'
-    else:
-        msg = '啟動動態偵測 `失敗` ！'
+    msg = '啟動動態偵測完成！' if result else '啟動動態偵測 `失敗` ！'
 
     if reply_token:
         send_reply_text_message(reply_token, msg)
     else:
         send_text_message(user_id, msg)
 
-def async_stop_detection_task(user_id: str, reply_token: str, camera_id: int):
-    stop_detection_task.apply_async(args=(user_id, reply_token, camera_id,))
-
 # 提供給 celery beat 呼叫使用
 @app.task
-def cron_stop_detection_task():
+def cron_start_detection_task():
     notify_user_id = permission._role_user_list_map.get(RoleType.admin)[0]
-    stop_detection_task(notify_user_id, None, 1)  # 目前只有一個 直接 hard code
+    camera_id = 1  # 目前只有一個 直接 hard code
+
+    svc = RealMotionService()
+    if svc.get_detection_status(camera_id) == MotionDetectionStatus.disable:
+        result = svc.set_detection_status(camera_id, MotionDetectionStatus.enable)
+        send_text_message(
+            notify_user_id,
+            '自動啟動動態偵測！' if result else '自動啟動動態偵測 `失敗` ！'    
+        )
+
+def async_stop_detection_task(user_id: str, reply_token: str, camera_id: int):
+    stop_detection_task.apply_async(args=(user_id, reply_token, camera_id,))
 
 @app.task
 def stop_detection_task(user_id: str, reply_token: Optional[str], camera_id: int):
     svc = RealMotionService()
     result = svc.set_detection_status(camera_id, MotionDetectionStatus.disable)
 
-    if result:
-        msg = '關閉動態偵測完成！'
-    else:
-        msg = '關閉動態偵測 `失敗` ！'
+    msg = '關閉動態偵測完成！' if result else '關閉動態偵測 `失敗` ！'
 
     if reply_token:
         send_reply_text_message(reply_token, msg)
     else:
         send_text_message(user_id, msg)
+
+# 提供給 celery beat 呼叫使用
+@app.task
+def cron_stop_detection_task():
+    notify_user_id = permission._role_user_list_map.get(RoleType.admin)[0]
+    camera_id = 1  # 目前只有一個 直接 hard code
+
+    svc = RealMotionService()
+    if svc.get_detection_status(camera_id) == MotionDetectionStatus.enable:
+        result = svc.set_detection_status(camera_id, MotionDetectionStatus.disable)
+        send_text_message(
+            notify_user_id,
+            '自動關閉動態偵測！' if result else '自動關閉動態偵測 `失敗` ！'
+        )
 
 def async_get_detection_status_task(user_id: str, reply_token: str, camera_id: int):
     get_detection_status_task.apply_async(args=(user_id, reply_token, camera_id,))
