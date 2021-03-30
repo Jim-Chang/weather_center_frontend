609d7f40858fc595461a9fd750dc2d9c85e4c1c4
Jim Chang <jim.chang@hardcoretech.co>
2021-03-30 17:56:57 +0800

    feat: add display last data

--- app/frontend/src/app/app.component.ts
+++ app/frontend/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 import { WeatherDataService } from 'Services/weather-data/weather-data.service';
 import { WeatherData } from 'Types/weather-data';
 import { mergeMap } from 'rxjs/operators';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +13,18 @@ import { mergeMap } from 'rxjs/operators';
 export class AppComponent implements OnInit {
 
   weatherDatas: WeatherData[] = [];
+  lastWeatherData: WeatherData = {
+    datetime: moment().format(),
+    temperature: 0,
+    humidity: 0,
+  };
 
   @ViewChild(FilterComponent, { static: true }) filter!: FilterComponent;
 
   constructor(private weatherDataService: WeatherDataService) {
     weatherDataService.getLatestWeatherData().subscribe((data) => {
       this.weatherDatas = data;
+      this.lastWeatherData = data[data.length - 1];
     });
   }
 
@@ -28,9 +35,16 @@ export class AppComponent implements OnInit {
       })
     ).subscribe((data) => {
       this.weatherDatas = data;
+      this.lastWeatherData = data[data.length - 1];
     }, () => {
       console.log('query weather data error');
     });
+  }
+
+  get lastDiffMinutes(): number {
+    const now = moment();
+    const last = moment(this.lastWeatherData.datetime);
+    return Math.round(moment.duration(now.diff(last)).asMinutes());
   }
 
 }
