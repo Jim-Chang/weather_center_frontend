3629792bd0ba02bf9cae8c6c4e1a96c5d882a961
Jim Chang <jim.chang@hardcoretech.co>
2021-04-14 16:31:54 +0800

    fix: date format

--- app/frontend/src/app/services/weather-data/weather-data.service.ts
+++ app/frontend/src/app/services/weather-data/weather-data.service.ts
@@ -3,15 +3,15 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { WeatherData } from 'Types/weather-data';
 import { DateRange } from 'Types/date-range';
 import { QueryWeatherDataResponse } from 'Types/weather-api-response';
-import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import * as moment from 'moment';
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherDataService {
 
-  private queryWeatherDataUrl = '/api/v1/weather/query';
+  private queryWeatherDataUrl = `/api/v1/weather/query`;
 
   constructor(private http: HttpClient) { }
 
@@ -20,15 +20,15 @@ export class WeatherDataService {
   }
 
   queryWeatherDataByHours(hours: number): Observable<WeatherData[]> {
-    const startDatetime = moment().subtract(hours, 'hours').format();
-    const endDatetime = moment().format();
+    const startDatetime = moment().subtract(hours, 'hours').format('YYYY-MM-DD HH:mm:ss');
+    const endDatetime = moment().format('YYYY-MM-DD HH:mm:ss');
 
     return this.queryWeatherData(startDatetime, endDatetime);
   }
 
   queryWeatherDataByDateRange(dateRange: DateRange): Observable<WeatherData[]> {
-    const startDatetime = this.formatDate(dateRange.startDate) + 'T00:00:00+08:00';
-    const endDatetime = this.formatDate(dateRange.endDate) + 'T23:59:59+08:00';
+    const startDatetime = this.formatDate(dateRange.startDate) + ' 00:00:00';
+    const endDatetime = this.formatDate(dateRange.endDate) + ' 23:59:59';
 
     return this.queryWeatherData(startDatetime, endDatetime);
   }
